b36b8cdb49b7548160b2640a897082d5a8ba2eba
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-03 21:51:54 +0300

    #3 - Fetch insurance plans data from server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./components/home/Home";
 import "./App.css";
 import MainContent from "./components/main/MainContent";
 import Footer from "./components/footer/Footer";
+import InsurancePlans from "./components/products/InsurancePlans";
+import axios from 'axios';
 
 function App() {
+  const url = "http://localhost:5125/api/v1/InsurancePlan/"
+
+  const[response, setResponse ] = useState("");
+
+
+  function getDataFromServer() {
+    axios.get(url)
+    .then((response) => {
+      console.log(response.data);
+      setResponse(response.data);
+    }) 
+    .catch((error)=>{
+      console.log("error " + error)
+    });
+  }
+
+  useEffect(()=>{
+    getDataFromServer();
+  }, []);
   return (
     <div className="App">
       <NavBar />
-      <Home/>
+      <Home />
       <MainContent />
+      <InsurancePlans response = {response}/>
       <Footer/>
     </div>
   );
