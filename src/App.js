5cb697c15db6e4df4e96b937c722a5b8fc436519
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-03 03:04:41 +0300

    #2 - Implement Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./components/navbar/NavBar";
+import Home from "./components/home/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home/>
     </div>
   );
 }
