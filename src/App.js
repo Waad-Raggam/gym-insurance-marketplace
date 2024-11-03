529fb7ba322cf1877dde7c2003be21ec8412bf09
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-03 19:15:35 +0300

    #2 - Update App.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./components/home/Home";
 import "./App.css";
+import MainContent from "./components/main/MainContent";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home/>
+      <MainContent />
     </div>
   );
 }
