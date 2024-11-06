6181ae1f7c6a9cb39e90b9ce8fb13618eb16858f
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-06 03:40:33 +0300

    #23 - Add error messages (registration)

--- src/components/user/UserRegistration.js
+++ src/components/user/UserRegistration.js
@@ -31,7 +31,27 @@ export default function UserRegistration() {
     axios
       .post(signUpUrl, userInformation)
       .then((response) => console.log(response, "new user"))
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        if (error.status === 400) {
+            if(error.response.data.errors.Name){
+                alert(error.response.data.errors.Name[0]);
+                return;
+            }
+            if(error.response.data.errors.Email){
+                alert(error.response.data.errors.Email[0]);
+                return;
+            }
+            if(error.response.data.errors.Password){
+                alert(error.response.data.errors.Password[0]);
+                return;
+            }
+            if(error.response.data.errors.PhoneNumber){
+                alert(error.response.data.errors.PhoneNumber[0]);
+                return;
+            }
+        }
+      });
   }
 
   return (
